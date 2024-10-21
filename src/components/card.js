cf45db122015d10479d508c70701ec0136b5927f
ArchiArchi <archi50235@gmail.com>
2024-10-20 23:05:25 -0700

    ready

--- src/components/card.js
+++ src/components/card.js
@@ -3,6 +3,16 @@ export function toggleLikeState(likeButton) {
   likeButton.classList.toggle('card__like-button_is-active');
 }
 
+// Initial cards array
+export const initialCards = [
+  { name: "Архыз", link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg" },
+  { name: "Челябинская область", link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg" },
+  { name: "Иваново", link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg" },
+  { name: "Камчатка", link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg" },
+  { name: "Холмогорский район", link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg" },
+  { name: "Байкал", link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg" }
+];
+
 // Function to create a card
 export function createCard(cardData, deleteCallback, openImagePopup, likeCallback) {
   const template = document.querySelector('#card-template').content.cloneNode(true);
--- src/components/index.js
+++ src/components/index.js
@@ -1,16 +1,7 @@
 import '../pages/index.css';
-import { createCard, removeCard, toggleLikeState } from './card.js';
+import { createCard, removeCard, toggleLikeState, initialCards } from './card.js';
 import { openModal, closeModal } from './modal.js';
 
-// Initial cards array
-const initialCards = [
-  { name: "Архыз", link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg" },
-  { name: "Челябинская область", link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg" },
-  { name: "Иваново", link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg" },
-  { name: "Камчатка", link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg" },
-  { name: "Холмогорский район", link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg" },
-  { name: "Байкал", link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg" }
-];
 
 // DOM elements
 const profileImage = document.querySelector('.profile__image');
