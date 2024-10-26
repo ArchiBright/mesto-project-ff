e03b78c7a376a02fce4b8beb15851b62304abf81
ArchiArchi <archi50235@gmail.com>
2024-10-25 22:41:11 -0700

    ready

--- src/components/card.js
+++ src/components/card.js
@@ -21,64 +21,56 @@ export function createCard(cardData, deleteCallback, openImagePopup, likeCallbac
   cardTitle.textContent = cardData.name;
   cardImage.src = cardData.link;
   cardImage.alt = cardData.name;
-  // likeCounter.textContent = cardData.likes.length;
+  likeCounter.textContent = cardData.likes.length;
 
-  // Event listener for image click to open image popup
   cardImage.addEventListener('click', () => {
-    openImagePopup(cardData.link, cardData.name, cardData.name); 
+    openImagePopup(cardData.link, cardData.name);
   });
 
-  // Fetch current user info and only after that decide whether to show the delete button
   getUserInfo()
-  .then(userData => {
-    const currentUserId = userData._id;
+    .then(userData => {
+      const currentUserId = userData._id;
 
-    // Check if the current user is the owner of the card
-    if (cardData.owner._id !== currentUserId) {
-      deleteButton.remove(); // Remove delete button if the current user is not the owner
-    } else {
-      deleteButton.addEventListener('click', () => {
-        openModal(deletePopup);
-        deleteConfirmButton.addEventListener('click', () => {
-          deleteCard(cardData._id)
-            .then(() => {
-              deleteCallback(cardElement);
-              closeModal(deletePopup);
-            })
-            .catch(err => {
-              console.log(err);
-            });
+      // Show or hide delete button based on ownership
+      if (cardData.owner._id !== currentUserId) {
+        deleteButton.remove();
+      } else {
+        deleteButton.addEventListener('click', () => {
+          openModal(deletePopup);
+          deleteConfirmButton.addEventListener('click', () => {
+            deleteCard(cardData._id)
+              .then(() => {
+                deleteCallback(cardElement);
+                closeModal(deletePopup);
+              })
+              .catch(err => console.log(err));
+          });
         });
+      }
+
+      // Initialize like button state based on whether the user has liked the card
+      const userHasLiked = cardData.likes.some(like => like._id === currentUserId);
+      if (userHasLiked) {
+        likeButton.classList.add('card__like-button_is-active');
+      }
+
+      // Event listener for like button to toggle like state
+      likeButton.addEventListener('click', () => {
+        toggleLike(cardData._id, userHasLiked)
+          .then(cardInfo => {
+            likeCounter.textContent = cardInfo.likes.length;
+
+            // Update button state based on new likes
+            if (cardInfo.likes.some(like => like._id === currentUserId)) {
+              likeButton.classList.add('card__like-button_is-active');
+            } else {
+              likeButton.classList.remove('card__like-button_is-active');
+            }
+          })
+          .catch(err => console.log("Failed to toggle like:", err));
       });
-    }
-
-    // Check if the current user has liked the card
-    const userHasLiked = cardData.likes.some(like => like._id === currentUserId);
-
-    // Call toggleLike function with card ID and userHasLiked status
-    toggleLike(cardData._id, userHasLiked)
-      .then(cardInfo => {
-        likeCounter.textContent = cardInfo.likes.length;
-
-        // Update like button state based on new data
-        if (cardInfo.likes.some(like => like._id === currentUserId)) {
-          likeButton.classList.add('card__like-button_is-active');
-        } else {
-          likeButton.classList.remove('card__like-button_is-active');
-        }
-      })
-      .catch(err => {
-        console.log("Failed to toggle like:", err);
-      });
-  })
-  .catch(err => {
-    console.log("Failed to get user info:", err);
-  });
-
-  // Event listener for liking a card
-  likeButton.addEventListener('click', () => {
-    likeCallback(likeButton);
-  });
+    })
+    .catch(err => console.log("Failed to get user info:", err));
 
   return cardElement;
 }
